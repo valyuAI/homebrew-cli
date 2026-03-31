13fad0be5c02f5f29956132397571bdcfbd5a340
yorkeccak <harveyyorke@proton.me>
2026-03-31 13:08:44 +0100

    Bump valyu to 1.0.3
    Fix deepresearch share URL path.

--- Formula/valyu.rb
+++ Formula/valyu.rb
@@ -1,26 +1,26 @@
 class Valyu < Formula
   desc "The search CLI for knowledge workers"
   homepage "https://github.com/valyuAI/valyu-cli"
-  version "1.0.2"
+  version "1.0.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.2/valyu-darwin-arm64.tar.gz"
-      sha256 "9c39fc00bbea4dd85305fc4707c386dc175b85ba24347a92079242eb36d6c785"
+      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.3/valyu-darwin-arm64.tar.gz"
+      sha256 "28979d5b2c153dc9e81a3e41e4045b0b40d84976d44f556a2af67a0ed2036aeb"
     else
-      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.2/valyu-darwin-x64.tar.gz"
-      sha256 "5ea74f7fb67e232e196d2efd194dd2d74e1e03cabfbac71116670488bc0d4661"
+      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.3/valyu-darwin-x64.tar.gz"
+      sha256 "44e05700abd8e799ee12e667f11bff222bd9b39161429127b21238306a1c0550"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.2/valyu-linux-arm64.tar.gz"
-      sha256 "080b9dd4a26eef81b050c2fab7e92ec776206143f7765e3559e359d7b158fcb4"
+      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.3/valyu-linux-arm64.tar.gz"
+      sha256 "fafa063c5c9ea6e1b9232898e4db79c557230005011a32c511388077c807fce1"
     else
-      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.2/valyu-linux-x64.tar.gz"
-      sha256 "1eafa1ab72e3717dc473c5f7b69d38cdea0fd81a4661ce2e0fc4265c40ad4690"
+      url "https://github.com/valyuAI/valyu-cli/releases/download/v1.0.3/valyu-linux-x64.tar.gz"
+      sha256 "b25b09428118b2d59c96b2d50f98ae32c105b9eac0df91048969d365cced517f"
     end
   end
 
